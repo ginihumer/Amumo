01293c252354aa4b91df8b9c71012225518205f9
Christina <ginihumer96@yahoo.de>
2023-05-18 11:05:29 -0400

    debug

--- clipexplorer/model.py
+++ clipexplorer/model.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 import os
 import json
-import CLOOB_local.clip.clip as cloob
+import ..CLOOB_local.clip.clip as cloob
 from CLOOB_local.clip.model import CLIPGeneral
 from CLOOB_local.cloob_training import model_pt, pretrained
 from torchvision import transforms
